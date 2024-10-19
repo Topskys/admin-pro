e76434f05216e23c0adc9614fc1c1b96767e366c
Topskys <2477350343@qq.com>
2024-10-19 16:41:30 +0800

    ✨ feat(路由): 整体导入路由模块文件

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,16 +2,14 @@ import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import "nprogress/nprogress.css";
 import NProgress from "nprogress";
 
+// 整体导入路由文件（默认是懒加载）
+const modules: Record<string, any> = import.meta.glob("./modules/*.ts", {
+  eager: true /* 取消懒加载 */,
+});
+
 // 配置路由
-const routes: Array<RouteRecordRaw> = [
-  {
-    path: "/",
-    name: "home",
-    component: () => import("../views/home/index.vue"),
-    meta: {},
-    children: [],
-  },
-];
+const routes: Array<RouteRecordRaw> = [];
+Object.keys(modules).forEach((key) => routes.push(modules[key].default));
 
 const router = createRouter({
   history: createWebHashHistory(),
@@ -21,12 +19,12 @@ const router = createRouter({
 export default router;
 
 router.beforeEach((to, from, next) => {
-    // 进度条开始 
+  // 进度条开始
   NProgress.start();
   next();
 });
 
 router.afterEach(() => {
-    // 进度条结束
+  // 进度条结束
   NProgress.done();
 });
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -5,5 +5,5 @@
 </template>
 
 <script setup lang="ts"></script>
-
+ 
 <style lang="less" scoped></style>
