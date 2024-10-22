d7b46426b3c1a8fc177ffb3b551c28a0e22e7d41
Topskys <2477350343@qq.com>
2024-10-22 11:35:23 +0800

    style: 初始化全局样式

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import "./styles/reset.css";
+import "./styles/index.css";
 import { registerECharts } from "./plugins/echarts";
 
 const app = createApp(App);
