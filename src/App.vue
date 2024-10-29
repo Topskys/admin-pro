7ad0f9684d8e45bc77fe3dcb899ea5b2c047e79f
Topskys <2477350343@qq.com>
2024-10-29 19:44:45 +0800

    refactor: 重构全局区域

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
+  <!-- 开启顶部安全区适配 -->
+  <van-nav-bar safe-area-inset-top />
   <!-- 路由占位 -->
   <router-view></router-view>
+  <!-- 开启底部安全区适配 -->
+  <van-number-keyboard safe-area-inset-bottom />
 </template>
 
 <script setup lang="ts"></script>
--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,15 +1,11 @@
 <template>
   <div class="layout">
-    <!-- 开启顶部安全区适配 -->
-    <van-nav-bar safe-area-inset-top />
     <router-view />
     <van-tabbar v-model="active" route>
       <van-tabbar-item replace to="/home" icon="home-o">首页</van-tabbar-item>
       <van-tabbar-item replace to="/search" icon="search">搜索</van-tabbar-item>
       <van-tabbar-item replace to="/setting" icon="setting-o">设置</van-tabbar-item>
     </van-tabbar>
-    <!-- 开启底部安全区适配 -->
-    <van-number-keyboard safe-area-inset-bottom />
   </div>
 </template>
 
