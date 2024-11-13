971373550c570e1786cfa883d314f013160d17cf
Topskys <2477350343@qq.com>
2024-11-13 21:21:23 +0800

    feat: 统计xhr请求（重写xhr）

---
+++ monitor/performance/xhr.js
@@ -0,0 +1,39 @@
+export const originalProto = XMLHttpRequest.prototype;
+export const originalOpen = originalProto.open;
+export const originalSend = originalProto.send;
+
+export default function xhr() {
+  overwriteOpenAndSend();
+}
+
+function overwriteOpenAndSend() {
+  originalProto.open = function newOpen(...args) {
+    this.method = args[0];
+    this.url = args[1];
+    return originalOpen.apply(this, args);
+  };
+  originalProto.send = function newSend(...args) {
+    this.startTime = Date.now();
+    const onLoaded = () => {
+      this.endTime = Date.now();
+      this.duration = this.endTime - this.startTime;
+      const { url, method, startTime, endTime, duration, status } = this;
+      const reportData = {
+        status,
+        duration,
+        endTime,
+        startTime,
+        url,
+        method: method.toUpperCase(),
+        type: 'performance',
+        success: status >= 200 && status < 300,
+        subType: 'xhr'
+      };
+      // TODO: 上报
+      this.removeEventListener('loadend', onLoaded, true);
+    };
+    // 监听xhr请求完成后触发onLoaded
+    this.addEventListener('loadend', onLoaded, true);
+    originalSend.apply(this, args);
+  };
+}
