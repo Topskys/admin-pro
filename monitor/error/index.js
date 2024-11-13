81504550d49b580bfce00c277eef32266ec46621
Topskys <2477350343@qq.com>
2024-11-13 21:40:28 +0800

    feat: 收集错误信息

---
+++ monitor/error/index.js
@@ -0,0 +1,51 @@
+export default function error() {
+  // 捕获资源加载失败的错误 js 、css、图片等
+  window.addEventListener(
+    'error',
+    function (e) {
+      const target = e.target;
+      if (!target) return;
+      if (target.src || target.href) {
+        const url = target.src || target.href;
+        const reportData = {
+          type: 'error',
+          subType: 'resource',
+          url: url,
+          html: target.outerHTML,
+          pageUrl: window.location.href,
+          paths: e.path,
+          startTime: e.timeStamp // 错误时间
+        };
+        // TODO: 上报
+      }
+    },
+    true
+  );
+  // 捕获js错误（同异步）
+  window.onerror = function (message, source, lineno, colno, error) {
+    const reportData = {
+      type: 'error',
+      subType: 'js',
+      url: source,
+      message: message,
+      source: source,
+      lineNo: lineno,
+      columnNo: colno,
+      stack: error.stack,
+      pageUrl: window.location.href,
+      startTime: error.timeStamp
+    };
+    // TODO: 上报
+  };
+  // 捕获promise、async/await错误
+  window.addEventListener('unhandledrejection', function (e) {
+    const reportData = {
+      type: 'error',
+      subType: 'promise',
+      reason: e.reason,
+      pageUrl: window.location.href,
+      startTime: e.timeStamp
+    };
+    // TODO: 上报
+  });
+}
