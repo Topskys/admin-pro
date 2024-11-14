af33b61b5a84516f168f2a636a8bd06daffa8823
Topskys <2477350343@qq.com>
2024-11-14 11:55:31 +0800

    test: 測試vue錯誤捕獲

--- src/App.vue
+++ src/App.vue
@@ -5,26 +5,33 @@
     <template #slot1>
       <p>detail name slot 具名插槽</p>
     </template>
-    <template #slot2="props">
+<template #slot2="props">
       <p>scope slot 作用域插槽-{{ props }}</p>
     </template>
-  </ParentComponent>
-  <div class="border"></div> -->
-  <VirtualList :list-data="items" :item-height="50" />
+</ParentComponent>
+<div class="border"></div> -->
+  <!-- <VirtualList :list-data="items" :item-height="50" /> -->
+  <div><button @click="jsErr">測試monitor捕獲vue框架錯誤</button></div>
 </template>
 
 <script setup lang="ts">
-import VirtualList from './features/VirtualList.vue';
-const items = ref(
-  Array.from({ length: 10000000 }, (_, i) => {
-    return {
-      id: i,
-      value: `Item ${i}`
-    };
-  })
-);
+// import VirtualList from './features/VirtualList.vue';
+// const items = ref(
+//   Array.from({ length: 10000000 }, (_, i) => {
+//     return {
+//       id: i,
+//       value: `Item ${i}`
+//     };
+//   })
+// );
 // import './features';
 // import ParentComponent from './features/parentComponent';
+
+const jsErr = () => {
+  const num = new Number(223.12);
+  throw new Error('js error');
+  // console.log(num.toFixed(01));
+};
 </script>
 <style lang="less">
 #app {
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,13 @@ import router from './router';
 import store from './store';
 import './styles/index.less';
 import { registerECharts } from './plugins/echarts';
+import monitor from '../monitor/dist/monitor.esm.js'; // 性能监控
 
 const app = createApp(App);
 app.use(router);
 app.use(store);
+app.use(monitor, {
+  url: 'http://localhost:8000/reportData'
+});
 registerECharts(app); // 注册echarts插件
 app.mount('#app');
