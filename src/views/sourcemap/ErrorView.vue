93e4d7874aea36d97465e916e157e67204d9ad17
Topskys <2477350343@qq.com>
2024-11-18 12:41:34 +0800

    fix: 移除原子样式

--- src/views/sourcemap/ErrorView.vue
+++ src/views/sourcemap/ErrorView.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <h1>Error</h1>
-    <button @click="triggerTypeError()">触发TypeError</button>
-    <button @click="triggerReferenceError">触发ReferenceError</button>
-    <button @click="triggerSyntaxError">触发SyntaxError</button>
+    <h1>Throw Error</h1>
+    <el-button @click="triggerTypeError()">触发TypeError</el-button>
+    <el-button @click="triggerReferenceError">触发ReferenceError</el-button>
+    <el-button @click="triggerSyntaxError">触发SyntaxError</el-button>
   </div>
 </template>
 <script setup lang="ts">
